60cf5b483a05fa330838432f9203bf1fdacb6846
yogesh munsah <yogeshmunsah07@gmail.com>
2024-02-01 20:17:13 +0400

    properties list in progress

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'your-cloud-name'
+  config.api_key = 'your-api-key'
+  config.api_secret = 'your-api-secret'
+  config.secure = true  # Use HTTPS by default
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
+  get 'home', to: 'pages#home', as: 'home'
 
   resources :properties do
     resources :booking
